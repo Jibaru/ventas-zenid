25d696c24323c4f127a56290859d5ee859bae383
Ignacio <ignacioruedaboada@hotmail.com>
2021-12-19 23:46:44 -0500

    Cambiado diseño

--- Shared/FormMensajeSistema.php
+++ Shared/FormMensajeSistema.php
@@ -14,16 +14,21 @@ class FormMensajeSistema
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mensaje Sistema</title>
-    <?php include_once("../layout/Estilos.php"); ?>
+    <link rel="stylesheet" href="../assets/lib/bootstrap-5.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/lib/fontawesome-free/css/all.min.css">
 </head>
 
-<body>
-    <main>
-        <section id="main">
-            <h1>Mensaje del sistema</h1>
-            <p><?php echo $mensaje ?></p>
-        </section>
-    </main>
+<body style="background-color: #98c0e3;">
+    <div class="container">
+        <div class="card w-50 shadow-sm bg-body rounded" style="margin: 5rem auto">
+            <div class="card-header text-center">
+                <h2>Mensaje del Sistema</h2>
+            </div>
+            <div class="card-body">
+                <p class="text-center"><?php echo $mensaje ?></p>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
